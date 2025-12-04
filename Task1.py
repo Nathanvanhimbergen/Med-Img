6cf72253dc0c03b666596e0598dadb6152a029fe
Nathan van Himbergen <n.w.vanhimbergen@student.tudelft.nl>
2025-12-04 11:48:03 +0100

    file loading implemented in Task1.py

--- Task1.py
+++ Task1.py
@@ -2,5 +2,19 @@ import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
-water = loadmat('data\dataset\task_1\water\hist_140.mat')
-fat = loadmat('data\dataset\task_1\fat\hist_140.mat')
+water = loadmat('data/dataset/task_1/water/hist_140.mat')
+fat = loadmat('data/dataset/task_1/fat/hist_140.mat')
+
+# Print all top-level variable names
+print(water.keys())
+# water counts (1, 130) and energy (1, 131)
+water_c = water['x']
+water_e = water['y']
+
+# Print all top-level variable names
+print(fat.keys())
+# fat counts (1, 130) and energy (1, 131)
+fat_c = fat['x']
+fat_e = fat['y']
+
+# plot histograms ...
